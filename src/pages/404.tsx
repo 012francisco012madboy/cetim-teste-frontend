da010d78979f5fcdd33dc2d5acb9c66adee76c53
Ariel Francisco <arielfrancisco690@gmail.com>
2026-08-19 22:06:50 +0100

    feat(not-found): adiciona página 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import { Container } from "@/components/layout/container"
+import {
+    Empty,
+    EmptyContent,
+    EmptyDescription,
+    EmptyHeader,
+    EmptyTitle,
+} from "@/components/ui/empty"
+
+export function Index404() {
+    return (
+        <Container>
+            <Empty>
+                <EmptyHeader>
+                    <EmptyTitle>404 - Página não encontrada</EmptyTitle>
+                    <EmptyDescription>
+                        A página que procuras não existe ou foi removida. Verifica o endereço
+                        ou volta à página inicial.
+                    </EmptyDescription>
+                </EmptyHeader>
+
+                <EmptyContent>
+                    <EmptyDescription>
+                        Desejas voltar? <a href="/">Página inicial</a>
+                    </EmptyDescription>
+                </EmptyContent>
+            </Empty>
+
+        </Container>
+    )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import Index from "../pages";
 import Product from "@/pages/product";
+import { Index404 } from "@/pages/404";
 
 const Rotas = () => {
     return (
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Index/>}/>
-            <Route path="/product/:id" element={<Product/>}/>
-        </Routes>
+            <Routes>
+                <Route path="*" element={<Index404 />} />
+                <Route path="/" element={<Index />} />
+                <Route path="/product/:id" element={<Product />} />
+            </Routes>
         </BrowserRouter>
     );
 }
